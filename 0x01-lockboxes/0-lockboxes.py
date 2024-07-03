a1b1ea590ac796e389684e32ec1cbf9f7eee71d9
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-07-03 18:00:48 +0000

    boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
-def look_next_opened_box(opened_boxes):
-    for index, box in opened_boxes.items():
-        if box.get('status') == 'opened':
-            box['status'] = 'opened/checked'
-            return box.get('keys')
-    return None
-
 def canUnlockAll(boxes):
-    if len(boxes) == 0:
-        return True
-
-    aux = {0: {'status': 'opened', 'keys': boxes[0]}}
-
-    while True:
-        keys = look_next_opened_box(aux)
-        if keys:
-            for key in keys:
-                if key < len(boxes) and key not in aux:
-                    aux[key] = {'status': 'opened', 'keys': boxes[key]}
-        elif 'opened' in [box.get('status') for box in aux.values()]:
-            continue
-        else:
-            break
-
-    return len(aux) == len(boxes)
-
-if __name__ == '__main__':
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Output: False
-
+    """
+    Function that checks with boolean value if the list type and
+    len to invoke two for iterations one to traverse the list
+    or not in order to open
+    """
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
