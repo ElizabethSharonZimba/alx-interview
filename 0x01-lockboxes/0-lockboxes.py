b9fe2a5962eecf36d2c153d24195dae39aed25e2
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-07-03 17:55:09 +0000

    boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
-def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-    
-    Parameters:
-    boxes (list of list of int): The list of boxes with keys inside them.
-    
-    Returns:
-    bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes:
-        return False
-    
-    n = len(boxes)
-    keys = {0}
-    opened = set()
-    
-    queue = [0]
-    
-    while queue:
-        current_box = queue.pop(0)
-        if current_box not in opened:
-            opened.add(current_box)
-            for key in boxes[current_box]:
-                if key not in keys and key < n:
-                    keys.add(key)
-                    queue.append(key)
-    
-    return len(opened) == n
+def look_next_opened_box(opened_boxes):
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
 
-if __name__ == "__main__":
+def canUnlockAll(boxes):
+    if len(boxes) == 0:
+        return True
+
+    aux = {0: {'status': 'opened', 'keys': boxes[0]}}
+
+    while True:
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                if key < len(boxes) and key not in aux:
+                    aux[key] = {'status': 'opened', 'keys': boxes[key]}
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        else:
+            break
+
+    return len(aux) == len(boxes)
+
+if __name__ == '__main__':
     boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
+    print(canUnlockAll(boxes))  # Output: True
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  #True
+    print(canUnlockAll(boxes))  # Output: True
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  #  False
+    print(canUnlockAll(boxes))  # Output: False
 
