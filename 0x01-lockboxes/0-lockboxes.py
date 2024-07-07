1a0f9f94a06a2895d7bbd2afaff1c65ad4f088e1
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-07-07 12:51:25 +0000

    last

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    """
-    unlockboxes
-    """
     total_boxes = len(boxes)
-    keys = [0]
-    counter = 0
-    i = 0
+    setofkeys = [0]
+    index = 0
 
-    while i < len(keys):
-        setkey = keys[i]
+    while index < len(setofkeys):
+        setkey = setofkeys[index]
         for key in boxes[setkey]:
-            if 0 < key < total_boxes and key not in keys:
-                keys.append(key)
-                counter += 1
-        i += 1
+            if 0 < key < total_boxes and key not in setofkeys:
+                setofkeys.append(key)
+        index += 1
 
-    return counter == total_boxes - 1
+    return len(setofkeys) == total_boxes
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
+
