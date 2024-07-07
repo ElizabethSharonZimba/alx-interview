9a30a8fdfaf5a8466ccc9e80873fbd7428a2bcaf
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-07-07 12:55:06 +0000

    eish

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    """
+    Determine if all the boxes can be opened
+    """
     total_boxes = len(boxes)
     setofkeys = [0]
     index = 0
@@ -17,9 +20,17 @@ if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
     print(canUnlockAll(boxes))  # True
 
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    boxes = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
     print(canUnlockAll(boxes))  # True
 
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    boxes = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[0]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [], [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6], [9, 5, 8, 8], [6, 2, 8, 6]]
     print(canUnlockAll(boxes))  # False
 
+    boxes = [[7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5]]
+    print(canUnlockAll(boxes))  # False
