0e87ee5d7b3907482d4d704432e728f4ad7dc7f1
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-09-04 17:20:01 +0000

    prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-def generatePrimeNumbers(limit):
+def isWinner(x, nums):
+    max_num = max(nums)
+    prime_numbers = sieve_of_eratosthenes(max_num)
     
-    primeNumbers = []
-    sieveList = [True] * (limit + 1)
-    for potentialPrime in range(2, limit + 1):
-        if sieveList[potentialPrime]:
-            primeNumbers.append(potentialPrime)
-            for multiple in range(potentialPrime, limit + 1, potentialPrime):
-                sieveList[multiple] = False
-    return primeNumbers
-
-
-def isWinner(numRounds, roundValues):
+    maria_wins = 0
+    ben_wins = 0
     
-    if not numRounds or not roundValues:
-        return None
-    mariaScore = benScore = 0
-    for i in range(numRounds):
-        primes = generatePrimeNumbers(roundValues[i])
-        if len(primes) % 2 == 0:
-            benScore += 1
+    for n in nums:
+        primes_in_round = [p for p in prime_numbers if p <= n]
+        current_player = "Mama"
+        
+        while primes_in_round:
+            prime = primes_in_round[0]
+            primes_in_round = [p for p in primes_in_round if p % prime != 0]
+            
+            if current_player == "Mama":
+                current_player = "Baby"
+            else:
+                current_player = "Mama"
+        
+        if current_player == "Baby":
+            maria_wins += 1
         else:
-            mariaScore += 1
-    if mariaScore > benScore:
-        return "Maria"
-    elif benScore > mariaScore:
-        return "Ben"
-    return None
+            ben_wins += 1
+    
+    if maria_wins > ben_wins:
+        return "Mama"
+    elif ben_wins > maria_wins:
+        return "Baby"
+    else:
+        return None
