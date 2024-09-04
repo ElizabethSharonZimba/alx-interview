32e7fef4daf76296746a97c68ff3f07775bf3258
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-09-04 17:26:06 +0000

    Update rotate_2d_matrix script

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,22 +5,23 @@ Rotate 2D Matrix
 
 
 def rotate_2d_matrix(matrix):
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
     """
-	rotate two dimension matrix 90 degrees clockwise
-    
-    """
-    n = len(matrix)
-    for i in range(int(n / 2)):
-        y = (n - i - 1)
-        for j in range(i, y):
-            x = (n - 1 - j)
-     
-            tmp = matrix[i][j]
-            
-            matrix[i][j] = matrix[x][i]
-            
-            matrix[x][i] = matrix[y][x]
-            
-            matrix[y][x] = matrix[j][y]
-            
-            matrix[j][y] = tmp
+    size = len(matrix)
+    for layer in range(int(size / 2)):
+        end = (size - layer - 1)
+        for index in range(layer, end):
+            opposite = (size - 1 - index)
+            # current number
+            temp = matrix[layer][index]
+            # change top for left
+            matrix[layer][index] = matrix[opposite][layer]
+            # change left for bottom
+            matrix[opposite][layer] = matrix[end][opposite]
+            # change bottom for right
+            matrix[end][opposite] = matrix[index][end]
+            # change right for top
+            matrix[index][end] = temp
+
---
+++ 0x07-rotate_2d_matrix/rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Test Rotate 2D Matrix
+"""
+
+from 0-rotate_2d_matrix import rotate_2d_matrix
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print("Original Matrix:")
+    for row in matrix:
+        print(row)
+
+    rotate_2d_matrix(matrix)
+
+    print("\nRotated Matrix:")
+    for row in matrix:
+        print(row)
+
